2891eaab0fccb1038f780847de1d728ce0eaf253
Saim Khan <saim.khan21@hotmail.com>
2022-06-15 07:55:38 +0100

    Initial Commit

---
+++ Cycling/CyclingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CyclingApp.swift
+//  Cycling
+//
+//  Created by Saim Khan on 15/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct CyclingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
